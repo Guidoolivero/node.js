0b56c5934f2b52fc28aa0c3cc17d86dba1ee8b62
GuidoOlivero <guidooliver2002@gmail.com>
2023-08-08 10:41:25 -0300

    Se crearon los archivos

---
+++ app.js
@@ -0,0 +1,13 @@
+const evenOrOdd = require('num-is-even-or-odd')
+
+
+console.log(evenOrOdd(2))
+console.log('buenas noches')
+// console.log("Hello World")
+
+// function sayHello(name) {
+//     console.log(`Hello ${name}`)
+// }
+
+// sayHello("Guido")
+
